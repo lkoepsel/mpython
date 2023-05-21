c3de0033fbff50f52bda7316c913279b713aa79f
Lief Koepsel <lkoepsel@wellys.com>
2023-05-21 09:21:37 -0700

    enhancing blink to allow for key interuption

--- blink.py
+++ blink.py
@@ -1,14 +1,27 @@
 from machine import Pin
 import time
+import sys
+import uselect
+
+
+spoll = uselect.poll()
+spoll.register(sys.stdin, uselect.POLLIN)
+
+
+def read1():
+    return(sys.stdin.read(1) if spoll.poll(0) else None)
 
 
 def Blink():
     led = Pin("LED", Pin.OUT)
+    key = read1()
 
-    while True:
+    while key is None:
         led.toggle()
-        time.sleep_ms(250)
+        time.sleep_ms(500)
+        key = read1()
 
 
 if __name__ == '__main__':
     Blink()
+    sys.exit()
